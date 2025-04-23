6c00184baa15a19b8265214a3eb66af4fc81b0cc
Lix <33119700+Lixeira@users.noreply.github.com>
2025-04-23 18:25:48 +0600

    v1.01

--- lixeira-linux-dnf-bash.sh
+++ lixeira-linux-dnf-bash.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+set -e
+
 # Demo packages
-INSTALL_PACKAGES=("htop" "git" "curl" "decibels")
-REMOVE_PACKAGES=("firefox" "baobab" "evince" "epiphany" "gnome-calendar" "gnome-clocks" "gnome-color-manager" "gnome-connections" "gnome-console" "gnome-contacts" "gnome-logs" "gnome-maps" "gnome-music" "gnome-remote-desktop" "gnome-shell-extensions" "gnome-tour" "gnome-user-docs" "gnome-user-share" "malcontent" "orca" "simple-scan" "yelp")
+INSTALL_PACKAGES=("htop" "git" "curl" "decibels" "gnome.papers")
+REMOVE_PACKAGES=(
+    "firefox" "baobab" "evince" "epiphany"
+    "gnome-calendar" "gnome-clocks" "gnome-color-manager"
+    "gnome-connections" "gnome-console" "gnome-contacts"
+    "gnome-logs" "gnome-maps" "gnome-music"
+    "gnome-remote-desktop" "gnome-shell-extensions"
+    "gnome-tour" "gnome-user-docs" "gnome-user-share"
+    "malcontent" "orca" "simple-scan" "yelp"
+)
 
 # Function to install packages
 install_packages() {
     echo "Starting installation of packages..."
     for PACKAGE in "${INSTALL_PACKAGES[@]}"; do
         echo "Installing $PACKAGE..."
-        sudo dnf install -y "$PACKAGE"
+        sudo dnf install -y "$PACKAGE" || echo "Failed to install $PACKAGE"
     done
     echo "Installation complete."
 }
@@ -19,7 +29,8 @@ remove_packages() {
     echo "Starting removal of packages..."
     for PACKAGE in "${REMOVE_PACKAGES[@]}"; do
         echo "Removing $PACKAGE..."
-        sudo dnf remove -y "$PACKAGE"
+        sudo dnf remove -y "$PACKAGE" || echo "Failed to remove $PACKAGE"
     done
     echo "Removal complete."
 }
+
